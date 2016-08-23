4d9f4b13ff7fdc146f0a228a51d137c82b90eccd
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-08-23 19:23:41 -0300

    testando gráficos

---
+++ src/br/com/jsf/sandbox/bean/VendasBean.java
@@ -0,0 +1,64 @@
+package br.com.jsf.sandbox.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
+
+import br.com.jsf.sandbox.dao.DAO;
+import br.com.jsf.sandbox.model.Livro;
+import br.com.jsf.sandbox.modelo.Venda;
+
+@ManagedBean
+@ViewScoped
+public class VendasBean {
+	
+	public BarChartModel getVendasModel() {
+		BarChartModel model = new BarChartModel();
+		        
+        ChartSeries vendaSerie2015 = new ChartSeries();
+        vendaSerie2015.setLabel("Vendas 2015");
+        
+        List<Venda> vendas = getVendas(1234);
+        for (Venda venda : vendas) {
+			vendaSerie2015.set(venda.getLivro().getTitulo(), venda.getQuantidade());
+		}
+        
+        model.addSeries(vendaSerie2015);
+        
+        
+        ChartSeries vendaSerie2016 = new ChartSeries();
+        vendaSerie2016.setLabel("Vendas 2016");
+      
+       vendas = getVendas(4321);
+        for (Venda venda : vendas) {
+			vendaSerie2016.set(venda.getLivro().getTitulo(), venda.getQuantidade());
+		}
+        
+        model.addSeries(vendaSerie2016);
+ 
+        
+         
+        return model;
+	}
+	
+	public List<Venda> getVendas(long seed) {
+		List<Livro> livros = new DAO<Livro>(Livro.class).listaTodos();
+		List<Venda> vendas = new ArrayList<Venda>();
+		
+		Random random = new Random(seed);
+		
+		for(Livro livro: livros) {
+			Integer quantidade = random.nextInt(200);
+			vendas.add(new Venda(livro, quantidade));
+		}
+		
+		return vendas;
+	}
+
+}
---
+++ src/br/com/jsf/sandbox/modelo/Venda.java
@@ -0,0 +1,32 @@
+package br.com.jsf.sandbox.modelo;
+
+import br.com.jsf.sandbox.model.Livro;
+
+public class Venda {
+
+	private Livro livro;
+
+	private Integer quantidade;
+
+	public Venda(Livro livro, Integer quantidade) {
+		this.livro = livro;
+		this.quantidade = quantidade;
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+}
